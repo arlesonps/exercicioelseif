8a2edf42c5c6a590344ecd527e0886294db1a9dd
Arleson Pontes <50966839+arlesonps@users.noreply.github.com>
2023-08-22 13:09:11 +0000

    exercicio usando for in, for of..

---
+++ forinof.js
@@ -0,0 +1,53 @@
+// const numeros = [3, 7, 2, 9, 5];
+// for (const i in numeros) {
+//     if (numeros.hasOwnProperty.call(numeros, i)) {
+//         const element = numeros[i];
+//     console.log(numeros[i]);
+//     }
+// }
+
+
+// const numeros = [3, 7, 2, 9, 5];
+// for (const numero of numeros) {
+//     console.log(numeros);
+// }
+
+// const objetoValores = {a: 10, b: 20, c: 30, d: 40};
+// let soma = 0;
+// for (let chave in objetoValores) {
+//     if (objetoValores.hasOwnProperty.call(objetoValores, chave)) {
+//         soma += objetoValores[chave];
+        
+//     }
+// }
+
+// console.log(" A soma dos valores é:", soma);
+
+// const numeros = [12, 5, 8, 21, 16, 7];
+// const numerosPares = [];
+
+// for (const numero of numeros) {
+//     if (numero % 2 === 0) {
+//         numerosPares.push(numero);
+//     } 
+//     }
+    
+// console.log("numeros pares:", numerosPares);
+
+// const estudantes = { alice: 18, bob: 20, carol: 19, david: 21};
+// for (const estudante in estudantes) {
+//     if (estudantes[estudante] === 19){
+//         console.log(`${estudante}: ${estudantes[estudante]} anos `);  
+//     }
+        
+//     }
+
+// const pontos = [ [2, 5, 8], [3, 9, 12], [4, 6, 10]];
+// let soma = 0;
+
+// for (let i = 0; i < pontos.length; i++) {
+//     for (let j = 0; j < pontos.length; j++) {
+//         soma += pontos[i][j];
+//     }   
+// }
+// console.log("A soma dos elementos da matriz é:", soma);
