d8f172abc0e140aa36d2a523f1222b2425be7426
Arleson Pontes <50966839+arlesonps@users.noreply.github.com>
2023-08-24 13:48:32 +0000

    lista e objeto

---
+++ listaobjeto.js
@@ -0,0 +1,70 @@
+// Criar um objeto vazio, chamado pessoa em javascript
+
+// var pessoa = {};
+
+// adicionar a propriedade "nome" com o valor "Joao" a um objeto chamado "pessoa".
+// const pessoa = {
+// }
+// pessoa.nome = "Joao";
+// console.log(pessoa);
+
+// acessar o valor da proriedade "idade" de um objeto chamado "usuario".
+
+// var idadeDoUsuario = usuario.idade;
+
+// Suponha que você tenha um objeto "produto" com a proprieade "preco" definida como 50. como você altera o valor dessa propriedade para 75.
+
+// let produto = {
+//     preco:50
+// }
+// produto.preco = 75;
+
+// console.log(produto);
+
+// Como remover a propriedade "email" de um objeto chamado "contato".
+
+//  let contato = {
+//     email: "exemplo@email.com"
+//  }
+//  delete contato.email;
+//  console.log(contato);
+
+// adicionar uma propriedade "endereço" com o valor "rua A, 123" a um objeto existente chamado "cliente".
+
+// let cliente ={
+// }
+// cliente.endereço = " Rua A, 123";
+// console.log(cliente);
+
+// Se você tem um objeto "empresa" com uma propriedade "departamentos", como você acessa o nome do primeiro departamento?
+ 
+// let empresa = {
+//     departamentos:[
+//      {nome: "RecursoHumano"}, 
+//      {nome: "Escritorio"},
+//      {nome: "Almoxerifado"},
+//     ]
+// };
+//  let nomePrimeiroDepartamento = empresa.departamentos[0].nome;
+// console.log(nomePrimeiroDepartamento);
+
+// suponha que você tenha um objeto "aluno" com propriedade "notas" contendo [85,90,78]. como voce altera a segunda nota para 95?
+
+// let aluno = {
+//     notas: [ 85, 90, 78]
+// };
+// aluno.notas[1] = 95;
+// console.log(aluno.notas);
+
+// let dados = {
+//     valores: [10,20,30]
+// };
+// dados.valores.splice(1, 1);
+// console.log(dados.valores);
+
+// let livro ={
+//     titulo: "Livro",
+//     autor: "Arleson"
+// }
+// console.log(livro.autor);
+// console.log(livro.titulo);
