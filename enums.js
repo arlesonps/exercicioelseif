bce54bff0b7e2870d1fc1baa834ac0cd81a9a2fd
Arleson Pontes <50966839+arlesonps@users.noreply.github.com>
2023-08-24 14:22:30 +0000

    exercicio enums

---
+++ enums.js
@@ -0,0 +1,32 @@
+// suponha que você está desenvolvendo um aplicativo de agendamengto e precisa representar os dias da semana. 
+// como você usaria enums para isso e qual seria o beneficio?
+//  const diasDaSemana = {
+//     DOMINGO: "Domingo",
+//     SEGUNDA: "Segunda-feira",
+//     TERCA: "Terça-feira",
+//     QUARTA: "Quarta-feira",
+//     QUINTA: "Quinta-feira",
+//     SEXTA: "Sexta-feira",
+//     SABADO: "Sabado",
+//  }
+//  const diaAtual = diasDaSemana.SEGUNDA;
+//  console.log("Hoje é", diaAtual);
+
+// Vocé está construindo um jogo e precisa representtar os diferentes tipos de inimigos. Como você usaria enums para essa situação.
+
+// const tipoDeInimigo = {
+//     ZUMBI: "Zumbi",
+//     ESQUELETO: "Esqueleto",
+//     DRAGAO: "Dragão",
+// }
+// function criarInimigo(nome,tipo) {
+//     return {
+//         nome: nome,
+//         tipo: tipo,
+//     };
+// }
+// const inimigo1 = criarInimigo("Fred", tipoDeInimigo.ZUMBI);
+// const inimigo2 = criarInimigo("Bones", tipoDeInimigo.ESQUELETO);
+
+// console.log(inimigo1);
+// console.log(inimigo2);
