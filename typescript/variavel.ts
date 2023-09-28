c6e5a4bebde8f1404f2dc2d6fc6e74c316859850
Arleson Pontes <50966839+arlesonps@users.noreply.github.com>
2023-09-28 13:46:46 +0000

    atualizações

---
+++ typescript/variavel.ts
@@ -0,0 +1,24 @@
+// //number
+let numero: number =42;
+
+// //null
+let nulo: null = null;
+
+//String
+
+let texto: string = "Olá mundo!";
+console.log(texto);
+
+
+//Boolean
+
+let booleano: boolean = true;
+
+// //Array
+
+let arrayDeNumeros: number[] = [1, 2, 3, 4, 5];
+
+// //Objeto
+
+let objeto: object = {chave:"valor"};
+
